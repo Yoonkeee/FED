c3e4190f31640b44d8bd84e103af8a753b28d25f
yoonki <yoonkeee@gmail.com>
2022-12-26 00:33:24 +0900

    Building Cleanser

--- libs/WantedCleanser.py
+++ libs/WantedCleanser.py
@@ -6,9 +6,16 @@ class Cleanser:
 
     raw_data: dict = {}
     cleansed_data: dict = {}
-    tech_stacks = ['react', 'javascript', 'python', 'java', 'kotlin', 'RDBMS']
+    tech_stacks = ['react', 'javascript', 'python', 'java', 'kotlin', 'RDBMS', 'spring',
+                   'vue.js', 'angular', 'node.js', 'django', 'flask', 'spring boot',
+                   'swift', 'flutter', 'objective-c', 'go', 'typescript', 'ruby', 'php',
+                   'c++', 'c#', 'scala', 'rust', 'haskell', 'erlang', 'elixir',
+                   'git', 'docker', 'jenkins', 'kubernetes', 'aws', 'gcp', 'azure',
+                   'redis', 'mongodb', 'mysql', 'postgresql', 'oracle', 'mariadb',
+                   'nosql']
 
     def __init__(self, raw_data: dict):
+        self.raw_data, self.cleansed_data = {}, {}
         if raw_data['is_valid']:
             self.raw_data = raw_data
         else:
@@ -17,9 +24,9 @@ class Cleanser:
 
     def cleanse(self):
         for data in self.raw_data['data']['requirements'].split('\n'):
-            print(data)
+            # print(data)
             for tech_stack in self.tech_stacks:
-                if tech_stack in data:
+                if tech_stack in data.lower():
                     self.cleansed_data[tech_stack] = True
 
 
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from libs import WantedParser, WantedCleanser
+from libs import WantedParser, WantedCleanser, WantedIdGenerator
 
-parse_test = WantedParser.Parser(86252)
+# parse_test = WantedParser.Parser(86252)
 # print(parse_test.get_raw_data())
-cleanse_test = WantedCleanser.Cleanser(parse_test.get_raw_data())
-cleanse_test.cleanse()
-print(cleanse_test.cleansed_data)
+
+generated_ids = WantedIdGenerator.IdGenerator().get_id_list()
+for article_id in generated_ids:
+    parse_test = WantedParser.Parser(article_id)
+    # print(parse_test.get_raw_data())
+    cleanse_test = WantedCleanser.Cleanser(parse_test.get_raw_data())
+    cleanse_test.cleanse()
+    if not cleanse_test.cleansed_data:
+        print(cleanse_test.raw_data['data']['requirements'])
+    print(article_id, cleanse_test.cleansed_data)
